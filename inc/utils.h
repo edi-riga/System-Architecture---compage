4d6be58beb9f0c3cb89a2d7433b21a2c4a4b6b72
rihards.novickis <rihards.novickis@edi.lv>
2020-07-16 10:48:16 +0300

    Remove default debug messages

--- inc/utils.h
+++ inc/utils.h
@@ -2,8 +2,13 @@
 #define _UTILS_H_
 
 #define _I(fmt, args...)  printf(fmt "\n", ##args)
-#define _D(fmt, args...)  printf("DEBUG: %s: " fmt "\n", __func__, ##args)
 #define _W(fmt, args...)  printf("WARNING: " fmt "\n", ##args)
 #define _E(fmt, args...)  fprintf(stderr, "ERROR: " fmt "\n", ##args)
+
+#if defined(DEBUG) or defined(COMPAGE_DEBUG)
+    #define _D(fmt, args...)  printf("DEBUG: %s: " fmt "\n", __func__, ##args)
+#else
+    #define _D(fmt, args...)
+#endif
 
 #endif
