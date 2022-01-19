47b2742d13704376ea437ce224a3ab6f4a498ea3
Rihards Novickis <rihards.novickis@edi.lv>
2022-01-19 21:23:16 +0200

    Added container_of macro

--- inc/compage_macro.h
+++ inc/compage_macro.h
@@ -16,4 +16,19 @@
 #define ARGUMENT_COUNT_PREPEND(_1, _2, _3, _4, _5, _6, _7, _8, _9, _10, _11, _12, _13, _14, N, ...) N
 #define ARGUMENT_INDEXES() 14, 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0
 
+/* The famous container_of macro, the macro retreives the member container
+ * structure when given a member of this function (reference name and address).
+ * Macro takes the following arguments
+ * - container_t   data type of the encapsulating structure
+ * - member_ref    name / reference of the member whoms address we are passing
+ * - member_addr   the address of the member
+ *
+ * For example, we can retreive compage_t from its pdata member with:
+ *     CONTAINER_OF(compage_t, pdata, pdata_ptr) */
+#define CONTAINER_OF(container_t, member_ref, member_addr) \
+((container_t*)( \
+  (uint64_t)member_addr - \
+  (uint64_t)(((container_t*)0)->member_ref) \
+))
+
 #endif
