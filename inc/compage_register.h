60ba81d1c66a8faaeb60cc40660a2fa19e341280
Rihards Novickis <rihards.novickis@edi.lv>
2022-02-11 13:09:55 +0200

    Made private component variables more unique

--- inc/compage_register.h
+++ inc/compage_register.h
@@ -8,41 +8,41 @@
 
 
 #define _COMPAGE_REGISTER_ID(id)                                               \
-compageId_t id##_id __attribute__((used,section("compage_ids"))) =             \
+compageId_t __##id##_id __attribute__((used,section("compage_ids"))) =         \
 {                                                                              \
   STRINGIFY(id)                                                                \
 };
 
 
 #define _COMPAGE_REGISTER_PDATA(id, pdata)                                     \
-compagePdata_t id##_pdata __attribute__((used,section("compage_pdata"))) =     \
+compagePdata_t __##id##_pdata __attribute__((used,section("compage_pdata"))) = \
 {                                                                              \
-  &id##_id,                                                                    \
+  &__##id##_id,                                                                \
   &pdata,                                                                      \
   sizeof(pdata)                                                                \
 };
 
 
 #define _COMPAGE_REGISTER_INIT(id, handler)                                    \
-compageInit_t id##_init __attribute__((used,section("compage_init"))) =        \
+compageInit_t __##id##_init __attribute__((used,section("compage_init"))) =    \
 {                                                                              \
-  &id##_id,                                                                    \
+  &__##id##_id,                                                                \
   (compageStatus_t(*)(void*))handler                                           \
 };
 
 
 #define _COMPAGE_REGISTER_LOOP(id, handler)                                    \
-compageLoop_t id##_loop __attribute__((used,section("compage_loop"))) =        \
+compageLoop_t __##id##_loop __attribute__((used,section("compage_loop"))) =    \
 {                                                                              \
-  &id##_id,                                                                    \
+  &__##id##_id,                                                                \
   (compageStatus_t(*)(void*))handler                                           \
 };
 
 
 #define _COMPAGE_REGISTER_EXIT(id, handler)                                    \
-compageExit_t id##_exit __attribute__((used,section("compage_exit"))) =        \
+compageExit_t __##id##_exit __attribute__((used,section("compage_exit"))) =    \
 {                                                                              \
-  &id##_id,                                                                    \
+  &__##id##_id,                                                                \
   (compageStatus_t(*)(void*))handler                                           \
 };
 
@@ -99,7 +99,7 @@ COMPAGE_CONCATENATE(COMPAGE_PDATA_ADD_CONFIGS_,N)(id, pdata, __VA_ARGS__)
 #define COMPAGE_PDATA_ADD_CONFIG_SINGLE(id, pdata, config)                     \
 compageConfig_t id##_config_##config __attribute__((section("compage_config"))) =\
 {                                                                              \
-  &id##_pdata,                                                                 \
+  &__##id##_pdata,                                                                 \
   STRINGIFY(config),                                                           \
   COMPAGE_TYPEID(((typeof(pdata)*)0)->config),                                 \
   (size_t)&((typeof(pdata)*)0)->config                                         \
