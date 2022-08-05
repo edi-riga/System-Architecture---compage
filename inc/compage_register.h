016cfb3912c58c945e6613c6ed37032164b31519
Rihards Novickis <rihards.novickis@edi.lv>
2022-08-05 17:38:28 +0300

    Modified generated init/exit/loop handler names to avoid potential conflicts

--- inc/compage_register.h
+++ inc/compage_register.h
@@ -24,7 +24,7 @@ compagePdata_t id##_pdata __attribute__((used,section("compage_pdata"))) =     \
 
 
 #define _COMPAGE_REGISTER_INIT(id, handler)                                    \
-compageInit_t id##_init __attribute__((used,section("compage_init"))) =        \
+compageInit_t id##_init_ __attribute__((used,section("compage_init"))) =        \
 {                                                                              \
   &id##_id,                                                                    \
   (compageStatus_t(*)(void*))handler                                           \
@@ -32,7 +32,7 @@ compageInit_t id##_init __attribute__((used,section("compage_init"))) =        \
 
 
 #define _COMPAGE_REGISTER_LOOP(id, handler)                                    \
-compageLoop_t id##_loop __attribute__((used,section("compage_loop"))) =        \
+compageLoop_t id##_loop_ __attribute__((used,section("compage_loop"))) =        \
 {                                                                              \
   &id##_id,                                                                    \
   (compageStatus_t(*)(void*))handler                                           \
@@ -40,7 +40,7 @@ compageLoop_t id##_loop __attribute__((used,section("compage_loop"))) =        \
 
 
 #define _COMPAGE_REGISTER_EXIT(id, handler)                                    \
-compageExit_t id##_exit __attribute__((used,section("compage_exit"))) =        \
+compageExit_t id##_exit_ __attribute__((used,section("compage_exit"))) =        \
 {                                                                              \
   &id##_id,                                                                    \
   (compageStatus_t(*)(void*))handler                                           \
